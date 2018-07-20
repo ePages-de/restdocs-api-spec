fece9e12f77e07cd22eee50684024ba1e7dad316
Alex Sandau <a.sandau@epages.com>
2018-07-20 10:16:13 +0200

    Improve openapi generator to handle multiple operations and responses

--- restdocs-openapi-gradle-plugin/src/test/kotlin/com/epages/restdocs/openapi/gradle/OpenApi20GeneratorTest.kt
+++ restdocs-openapi-gradle-plugin/src/test/kotlin/com/epages/restdocs/openapi/gradle/OpenApi20GeneratorTest.kt
@@ -28,61 +28,164 @@ class OpenApi20GeneratorTest {
         println(Json.pretty().writeValueAsString(openapi))
     }
 
+    @Test
+    fun `should convert multiple resource models to openapi`() {
+        val api = givenResourceModelsWithDifferentResponsesForSameRequest()
+
+        val openapi = OpenApi20Generator.generate(api)
+
+        println(Json.pretty().writeValueAsString(openapi))
+    }
+
     private fun givenOneResourceModel(): List<ResourceModel> {
         return listOf(
             ResourceModel(
                 operationId = "test",
                 privateResource = false,
                 deprecated = false,
-                request = RequestModel(
-                    path = "/products/{id}",
-                    method = "GET",
-                    securityRequirements = SecurityRequirements(
-                        type = OAUTH2
-                    ),
-                    headers = listOf(),
-                    pathParameters = listOf(
-                        ParameterDescriptor(
-                            name = "id",
-                            description = "Product ID",
-                            type = "STRING",
-                            optional = false,
-                            ignored = false
-                        )
-                    ),
-                    requestParameters = listOf(
-                        ParameterDescriptor(
-                            name = "locale",
-                            description = "Localizes the product fields to the given locale code",
-                            type = "STRING",
-                            optional = true,
-                            ignored = false
-                        )
-                    ),
-                    requestFields = listOf()
+                request = getProductRequest(),
+                response = getProduct200Response(getProduct200ResponseExample())
+            )
+        )
+    }
+
+    private fun givenResourceModelsWithDifferentResponsesForSameRequest() : List<ResourceModel> {
+        return listOf(
+                ResourceModel(
+                        operationId = "test",
+                        privateResource = false,
+                        deprecated = false,
+                        request = getProductRequest(),
+                        response = getProduct200Response(getProduct200ResponseExample())
                 ),
-                response = ResponseModel(
-                    status = 200,
-                    contentType = "application/json",
-                    headers = listOf(),
-                    responseFields = listOf(
+                ResourceModel(
+                        operationId = "test",
+                        privateResource = false,
+                        deprecated = false,
+                        request = getProductRequest(),
+                        response = getProduct200Response(getProduct200ResponseAlternateExample())
+                ),
+                ResourceModel(
+                        operationId = "test",
+                        privateResource = false,
+                        deprecated = false,
+                        request = getProductRequest(),
+                        response = getProduct400Response()
+                ),
+                ResourceModel(
+                        operationId = "test",
+                        privateResource = false,
+                        deprecated = false,
+                        request = deleteProductRequest(),
+                        response = deleteProduct204Response()
+                )
+        )
+    }
+
+    private fun getProduct200Response(example: String): ResponseModel {
+        return ResponseModel(
+                status = 200,
+                contentType = "application/json",
+                headers = listOf(),
+                responseFields = listOf(
                         FieldDescriptor(
-                            path = "_id",
-                            description = "ID of the product",
-                            type = "STRING"
+                                path = "_id",
+                                description = "ID of the product",
+                                type = "STRING"
                         ),
                         FieldDescriptor(
-                            path = "description",
-                            description = "Product description, localized.",
-                            type = "STRING"
+                                path = "description",
+                                description = "Product description, localized.",
+                                type = "STRING"
                         )
-                    ),
-                    example = "{\n" +
-                            "    \"_id\": \"123\",\n" +
-                            "    \"description\": \"Good stuff!\"\n" +
-                            "}"
-                )
-            )
+                ),
+                example = example
+        )
+    }
+
+    private fun getProduct400Response(): ResponseModel {
+        return ResponseModel(
+                status = 400,
+                contentType = "application/json",
+                headers = listOf(),
+                responseFields = listOf(),
+                example = "This is an ERROR!"
+        )
+    }
+
+    private fun getProduct200ResponseExample(): String {
+        return "{\n" +
+                "    \"_id\": \"123\",\n" +
+                "    \"description\": \"Good stuff!\"\n" +
+                "}"
+    }
+
+    private fun getProduct200ResponseAlternateExample(): String {
+        return "{\n" +
+                "    \"_id\": \"123\",\n" +
+                "    \"description\": \"Bad stuff!\"\n" +
+                "}"
+    }
+
+    private fun getProductRequest(): RequestModel {
+        return RequestModel(
+                path = "/products/{id}",
+                method = "GET",
+                securityRequirements = SecurityRequirements(
+                        type = OAUTH2
+                ),
+                headers = listOf(),
+                pathParameters = listOf(
+                        ParameterDescriptor(
+                                name = "id",
+                                description = "Product ID",
+                                type = "STRING",
+                                optional = false,
+                                ignored = false
+                        )
+                ),
+                requestParameters = listOf(
+                        ParameterDescriptor(
+                                name = "locale",
+                                description = "Localizes the product fields to the given locale code",
+                                type = "STRING",
+                                optional = true,
+                                ignored = false
+                        )
+                ),
+                requestFields = listOf()
+        )
+    }
+
+    private fun deleteProductRequest(): RequestModel {
+        return RequestModel(
+                path = "/products/{id}",
+                method = "DELETE",
+                securityRequirements = SecurityRequirements(
+                        type = OAUTH2
+                ),
+                headers = listOf(),
+                pathParameters = listOf(
+                        ParameterDescriptor(
+                                name = "id",
+                                description = "Product ID",
+                                type = "STRING",
+                                optional = false,
+                                ignored = false
+                        )
+                ),
+                requestParameters = listOf(),
+                requestFields = listOf()
+        )
+    }
+
+    private fun deleteProduct204Response(): ResponseModel {
+        return ResponseModel(
+                status = 204,
+                contentType = "application/json",
+                headers = listOf(),
+                responseFields = listOf(),
+                example = ""
         )
     }
 }
