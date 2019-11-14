f39d1e6ef871dd473f558b0de34a3c90a58aaa6f
Oleksandr Abasov <oleksandra@tradeix.com>
2019-11-14 11:39:05 +0200

    Fix test OpenApi20GeneratorTest - added default value to status description

--- restdocs-api-spec-model/src/main/kotlin/com/epages/restdocs/apispec/model/ResourceModel.kt
+++ restdocs-api-spec-model/src/main/kotlin/com/epages/restdocs/apispec/model/ResourceModel.kt
@@ -39,7 +39,7 @@ data class RequestModel(
 
 data class ResponseModel(
     val status: Int,
-    val statusDescription: String?,
+    val statusDescription: String? = null,
     val contentType: String?,
     val headers: List<HeaderDescriptor>,
     val responseFields: List<FieldDescriptor>,
