388fe3289a81fd11772e969ab630c2ebe57c55b6
Jan Mewes <jan.mewes@epages.com>
2021-03-15 14:03:38 +0100

    WIP

--- restdocs-api-spec-webtestclient/build.gradle.kts
+++ restdocs-api-spec-webtestclient/build.gradle.kts
@@ -33,6 +33,30 @@ publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
+
+            pom {
+                name.set("REST Doc API Spec - Web Test Client")
+                description.set("Adds API specification support to Spring REST Docs ")
+                url.set("https://github.com/ePages-de/restdocs-api-spec")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/ePages-de/restdocs-api-spec/blob/master/LICENSE")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("ePages")
+                        name.set("ePages Devs")
+                        email.set("info@epages.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/ePages-de/restdocs-api-spec.git")
+                    developerConnection.set("scm:git:ssh://github.com/ePages-de/restdocs-api-spec.git")
+                    url.set("https://github.com/ePages-de/restdocs-api-spec")
+                }
+            }
         }
     }
 }
