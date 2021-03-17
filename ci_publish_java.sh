42b51c91ff243f489042da8a1e8777323b95e8f3
Jan Mewes <jan.mewes@epages.com>
2021-03-17 11:39:22 +0100

    GH-153: Stop publish Gradle plugin to Sonatype

--- ci_publish_java.sh
+++ ci_publish_java.sh
@@ -7,9 +7,10 @@ openssl aes-256-cbc -K $encrypted_7b7bcfd5be68_key -iv $encrypted_7b7bcfd5be68_i
   -d
 
 ./gradlew publishToSonatype \
-	--info \
-	-Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
-	-Dorg.gradle.project.sonatypePassword="${SONATYPE_PASSWORD}" \
- 	-Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
-	-Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
-	-Dorg.gradle.project.signing.secretKeyRingFile="${SIGNING_KEYRING_FILE}"
+  --info \
+  --exclude-task :restdocs-api-spec-gradle-plugin:publishToSonatype \
+  -Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
+  -Dorg.gradle.project.sonatypePassword="${SONATYPE_PASSWORD}" \
+  -Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
+  -Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
+  -Dorg.gradle.project.signing.secretKeyRingFile="${SIGNING_KEYRING_FILE}"
