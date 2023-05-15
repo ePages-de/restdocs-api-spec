f40db3bb2fcc97f8d03e5b6548978a8c5c58c380
Jan Mewes <jan.mewes@epages.com>
2023-05-15 14:58:51 +0200

    Add testing support

--- ci_publish_java.sh
+++ ci_publish_java.sh
@@ -5,6 +5,53 @@ set -e # Exit with nonzero exit code if anything fails
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 SECRET_KEYS_FILE="${SCRIPT_DIR}/secret-keys.gpg"
 
+###############################################################################
+# Parameter handling
+###############################################################################
+
+usage () {
+     cat << EOF
+DESCRIPTION:
+The script publishes the Java libraries of this project to Sonatype or
+Maven Local (default).
+ 
+SYNOPSIS:
+$0 [-s] [-h]
+ 
+OPTIONS:
+    -s   Publish to Sonatype     (Default: off)
+    -h   Show this message.
+    -?   Show this message.
+
+REQUIRED ENVIRONMENT VARIABLES:
+- FILE_ENCRYPTION_PASSWORD: Passphrase for decrypting the signing keys
+- SIGNING_KEY_ID
+- SIGNING_PASSWORD
+- SONATYPE_USERNAME
+- SONATYPE_PASSWORD
+
+DEPENDENCIES:
+- gpg: https://help.ubuntu.com/community/GnuPrivacyGuardHowto
+
+EOF
+}
+
+while getopts "s h ?" option ; do
+     case $option in
+          s)   PUBLISH_TO_SONATYPE='true'
+               ;;
+          h )  usage
+               exit 0;;
+          ? )  usage
+               exit 0;;
+     esac
+done
+ 
+
+###############################################################################
+# Env variables and dependencies
+###############################################################################
+
 function check_variable_set() {
   _VARIABLE_NAME=$1
 	_VARIABLE_VALUE=${!_VARIABLE_NAME}
@@ -19,6 +66,15 @@ check_variable_set SIGNING_PASSWORD
 check_variable_set SONATYPE_USERNAME
 check_variable_set SONATYPE_PASSWORD
 
+if ! command -v gpg &> /dev/null; then
+  echo "gpg not installed. See https://help.ubuntu.com/community/GnuPrivacyGuardHowto"
+  exit 1
+fi
+
+###############################################################################
+# Parameter handling
+###############################################################################
+
 # Decrypt signing key
 gpg --quiet --batch --yes --decrypt --passphrase="${FILE_ENCRYPTION_PASSWORD}" \
 	--output ${SECRET_KEYS_FILE} secret-keys.gpg.enc
@@ -28,8 +84,15 @@ if [[ ! -f "${SECRET_KEYS_FILE}" ]]; then
 	exit 1
 fi
 
+# Determine where to publish the Java archives
+if [[ "${PUBLISH_TO_SONATYPE}" == "true" ]]; then
+	PUBLISH_GRADLE_TASK="publishToSonatype"
+else
+	PUBLISH_GRADLE_TASK="publishToMavenLocal"
+fi
+
 # Publish
-./gradlew publishToSonatype \
+./gradlew ${PUBLISH_GRADLE_TASK} \
 	--info \
   --exclude-task :restdocs-api-spec-gradle-plugin:publishToSonatype \
 	-Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
