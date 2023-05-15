4f54d2e446f0701a7e0e60724caa6e38ea96f3f4
Jan Mewes <jan.mewes@epages.com>
2023-05-15 22:48:38 +0900

    Add GitHub Actions and Sonar support (#237)
    GH-235
    
    (cherry picked from commit ac1600f4b6d552bdab8b7c71e9cea866fa40a57d)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,18 @@
 
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.kt3k.gradle.plugin.CoverallsPluginExtension
 import pl.allegro.tech.build.axion.release.domain.TagNameSerializationConfig
 import pl.allegro.tech.build.axion.release.domain.hooks.HooksConfig
 
 
 plugins {
-    id("com.github.kt3k.coveralls") version "2.12.0"
+    `maven-publish`
     id("io.github.gradle-nexus.publish-plugin") version "1.0.0"
     id("org.jmailen.kotlinter") version "3.3.0" apply false
+    id("org.sonarqube") version "4.0.0.2929"
     id("pl.allegro.tech.build.axion-release") version "1.9.2"
     jacoco
     java
-    kotlin("jvm") version "1.4.20" apply false
-    `maven-publish`
+    kotlin("jvm") version "1.7.22" apply false
 }
 
 repositories {
@@ -58,12 +57,12 @@ allprojects {
 subprojects {
 
     val jacksonVersion by extra { "2.12.2" }
-    val springBootVersion by extra { "2.1.9.RELEASE" }
-    val springRestDocsVersion by extra { "2.0.4.RELEASE" }
+    val springBootVersion by extra { "3.0.2" }
+    val springRestDocsVersion by extra { "3.0.0" }
     val junitVersion by extra { "5.4.2" }
 
     tasks.withType<KotlinCompile> {
-        kotlinOptions.jvmTarget = "1.8"
+        kotlinOptions.jvmTarget = "17"
     }
 
     tasks.withType<Test> {
@@ -83,12 +82,6 @@ subprojects {
             }
         }
     }
-}
-
-//coverall multi module plugin configuration starts here
-configure<CoverallsPluginExtension> {
-    sourceDirs = nonSampleProjects.flatMap { it.sourceSets["main"].allSource.srcDirs }.filter { it.exists() }.map { it.path }
-    jacocoReportPath = "$buildDir/reports/jacoco/jacocoRootReport/jacocoRootReport.xml"
 }
 
 tasks {
@@ -115,11 +108,19 @@ tasks {
             xml.isEnabled = true
         }
     }
-    getByName("coveralls").dependsOn(jacocoRootReport)
+    getByName("sonar").dependsOn(jacocoRootReport)
 }
 
 nexusPublishing {
     repositories {
         sonatype ()
+    }
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "ePages-de_restdocs-api-spec")
+        property("sonar.organization", "epages-de")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
--- ci_publish_java.sh
+++ ci_publish_java.sh
@@ -1,16 +1,102 @@
 #!/bin/bash
-set -e
 
-openssl aes-256-cbc -K $encrypted_7b7bcfd5be68_key -iv $encrypted_7b7bcfd5be68_iv \
-  -in secret-keys.gpg.enc \
-  -out "${SIGNING_KEYRING_FILE}" \
-  -d
+set -e # Exit with nonzero exit code if anything fails
 
-./gradlew publishToSonatype \
-  --info \
-  --exclude-task :restdocs-api-spec-gradle-plugin:publishToSonatype \
-  -Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
-  -Dorg.gradle.project.sonatypePassword="${SONATYPE_PASSWORD}" \
-  -Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
-  -Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
-  -Dorg.gradle.project.signing.secretKeyRingFile="${SIGNING_KEYRING_FILE}"
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SECRET_KEYS_FILE="${SCRIPT_DIR}/secret-keys.gpg"
+
+###############################################################################
+# Parameter handling
+###############################################################################
+
+usage () {
+     cat << EOF
+DESCRIPTION:
+The script publishes the Java libraries of this project to Sonatype or
+Maven Local (default).
+ 
+SYNOPSIS:
+$0 [-s] [-h]
+ 
+OPTIONS:
+    -s   Publish to Sonatype     (Default: off)
+    -h   Show this message.
+    -?   Show this message.
+
+REQUIRED ENVIRONMENT VARIABLES:
+- FILE_ENCRYPTION_PASSWORD: Passphrase for decrypting the signing keys
+- SIGNING_KEY_ID
+- SIGNING_PASSWORD
+- SONATYPE_USERNAME
+- SONATYPE_PASSWORD
+
+DEPENDENCIES:
+- gpg: https://help.ubuntu.com/community/GnuPrivacyGuardHowto
+
+EOF
+}
+
+while getopts "s h ?" option ; do
+     case $option in
+          s)   PUBLISH_TO_SONATYPE='true'
+               ;;
+          h )  usage
+               exit 0;;
+          ? )  usage
+               exit 0;;
+     esac
+done
+ 
+
+###############################################################################
+# Env variables and dependencies
+###############################################################################
+
+function check_variable_set() {
+  _VARIABLE_NAME=$1
+	_VARIABLE_VALUE=${!_VARIABLE_NAME}
+	if [[ -z ${_VARIABLE_VALUE} ]]; then
+		echo "Missing env variable ${_VARIABLE_NAME}"
+		exit 1
+	fi
+}
+check_variable_set FILE_ENCRYPTION_PASSWORD
+check_variable_set SIGNING_KEY_ID
+check_variable_set SIGNING_PASSWORD
+check_variable_set SONATYPE_USERNAME
+check_variable_set SONATYPE_PASSWORD
+
+if ! command -v gpg &> /dev/null; then
+  echo "gpg not installed. See https://help.ubuntu.com/community/GnuPrivacyGuardHowto"
+  exit 1
+fi
+
+###############################################################################
+# Parameter handling
+###############################################################################
+
+# Decrypt signing key
+gpg --quiet --batch --yes --decrypt --passphrase="${FILE_ENCRYPTION_PASSWORD}" \
+	--output ${SECRET_KEYS_FILE} secret-keys.gpg.enc
+
+if [[ ! -f "${SECRET_KEYS_FILE}" ]]; then
+	echo "File ${SECRET_KEYS_FILE} does not exist"
+	exit 1
+fi
+
+# Determine where to publish the Java archives
+if [[ "${PUBLISH_TO_SONATYPE}" == "true" ]]; then
+	PUBLISH_GRADLE_TASK="publishToSonatype"
+else
+	PUBLISH_GRADLE_TASK="publishToMavenLocal"
+fi
+
+# Publish
+./gradlew ${PUBLISH_GRADLE_TASK} \
+	--info \
+     --exclude-task :restdocs-api-spec-gradle-plugin:publishToSonatype \
+	-Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
+	-Dorg.gradle.project.sonatypePassword="${SONATYPE_PASSWORD}" \
+	-Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
+	-Dorg.gradle.project.signing.password="${SIGNING_PASSWORD}" \
+	-Dorg.gradle.project.signing.secretKeyRingFile="${SECRET_KEYS_FILE}"
