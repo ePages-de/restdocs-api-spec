0093ecc251ded4e90ecc9b7bd7c8817eb2b92ef9
Jan Mewes <jan.mewes@epages.com>
2023-05-15 15:45:09 +0200

    Fix OutOfMemory error
    https://community.sonarsource.com/t/kotlin-project-scan-fails-with-outofmemory-for-4-files/48127

--- ci_publish_java.sh
+++ ci_publish_java.sh
@@ -94,7 +94,7 @@ fi
 # Publish
 ./gradlew ${PUBLISH_GRADLE_TASK} \
 	--info \
-  --exclude-task :restdocs-api-spec-gradle-plugin:publishToSonatype \
+     --exclude-task :restdocs-api-spec-gradle-plugin:publishToSonatype \
 	-Dorg.gradle.project.sonatypeUsername="${SONATYPE_USERNAME}" \
 	-Dorg.gradle.project.sonatypePassword="${SONATYPE_PASSWORD}" \
 	-Dorg.gradle.project.signing.keyId="${SIGNING_KEY_ID}" \
