ac1600f4b6d552bdab8b7c71e9cea866fa40a57d
Jan Mewes <jan.mewes@epages.com>
2023-05-15 15:48:38 +0200

    Add GitHub Actions and Sonar support (#237)
    GH-235

--- ci_publish_gradle.sh
+++ ci_publish_gradle.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 set -e
 
+function check_variable_set() {
+  _VARIABLE_NAME=$1
+	_VARIABLE_VALUE=${!_VARIABLE_NAME}
+	if [[ -z ${_VARIABLE_VALUE} ]]; then
+		echo "Missing env variable ${_VARIABLE_NAME}"
+		exit 1
+	fi
+}
+check_variable_set GRADLE_PUBLISH_KEY
+check_variable_set GRADLE_PUBLISH_SECRET
+
 ./gradlew publishPlugins -p restdocs-api-spec-gradle-plugin
