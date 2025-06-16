4b8fd3c73f61ec10bd8cf346b071c2f24265563c
Wojciech Odrobina <wodrobina@epages.com>
2025-06-16 08:47:28 +0200

    EPNG-12229 Use stonatype with staging API url (#285)

--- build.gradle.kts
+++ build.gradle.kts
@@ -113,7 +113,10 @@ tasks {
 
 nexusPublishing {
     repositories {
-        sonatype ()
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+        }
     }
 }
 
