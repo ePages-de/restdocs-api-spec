6c13e19c4e80829c4e2ef0735a51535910384933
Wojciech Odrobina <wodrobina@epages.com>
2026-07-30 11:16:47 +0200

    Removed @Validated

--- samples/restdocs-api-spec-sample/src/main/java/com/epages/restdocs/apispec/sample/ProductSearchController.java
+++ samples/restdocs-api-spec-sample/src/main/java/com/epages/restdocs/apispec/sample/ProductSearchController.java
@@ -26,7 +26,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/product-search")
-@Validated
 public class ProductSearchController {
 
     private final ProductRepository productRepository;
